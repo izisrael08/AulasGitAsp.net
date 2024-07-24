f0aa97359e3fc20ae388eb86fdb5196bd46bf714
israelguedes008@gmail.com <israelguedes008@outlook.com>
2024-07-24 01:26:26 -0300

    linkando as coisas

--- ControleDeContatos/Controllers/ContatoController.cs
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -8,5 +8,20 @@ namespace ControleDeContatos.Controllers
         {
             return View();
         }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
     }
 }
