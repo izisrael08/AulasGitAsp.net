2be3a6a511a4ecd5d3a9217334e4ce2881aa90d5
israelguedes008@gmail.com <israelguedes008@outlook.com>
2024-07-24 23:38:57 -0300

    falta parte de banco

---
+++ ControleDeContatos/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos.Data
+{
+    public class BancoContext : DbContext
+    { //Criar o Construtor
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
---
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+    }
+}
